eb7c3851bd03d1a42708ef369db457672da511cd
chr211 <tauboo23@email.arizona.edu>
2025-04-24 14:23:50 -0700

    Delete environmentvars.sh

--- environmentvars.sh
+++
@@ -1,5 +0,0 @@
-
-setx URLSCANAPIKEY "b6306461-7477-412f-948a-a9c7021c86a5"
-
-echo The URLSCANAPIKEY environment variable was set globally. You might need to restart your system or IDE for the changes to take effect.
-
